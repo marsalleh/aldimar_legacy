daf9b03eb4b3bc9fa6bc8523b04557f0d79e95e8
Marsalleh <88478123+marsalleh@users.noreply.github.com>
2026-01-12 02:57:51 +0800

    Delete db_config_upload.php

--- db_config_upload.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "u715342185_aldimar_user", "M4rs030128", "u715342185_aldimar_db");
-if ($conn->connect_error)
-    die("Connection failed: " . $conn->connect_error);
-?>
